7ffdf0aeef5358762cdae2ee239ebc9cc105ea70
Jasio <mcyanek@gmail.com>
2020-11-02 10:55:35 +0100

    implements spring project structure

---
+++ src/main/java/com/codecool/GamLib/GamLibApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.GamLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/GamLib/GamLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.GamLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
