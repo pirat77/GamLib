09310e019eadf37b308bf379398a54745f863886
Jasio <mcyanek@gmail.com>
2020-11-03 06:22:40 +0100

    removes unnecessary code

--- src/main/java/com/codecool/GamLib/repositories/GameRepository.java
+++ src/main/java/com/codecool/GamLib/repositories/GameRepository.java
@@ -2,13 +2,11 @@ package com.codecool.GamLib.repositories;
 
 import com.codecool.GamLib.model.Game;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.core.CrudMethods;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import java.lang.reflect.Method;
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public class GameRepository {
