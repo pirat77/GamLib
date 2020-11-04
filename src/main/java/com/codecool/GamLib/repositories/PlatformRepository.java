b11efe42315deaf863aaaedb91037380e03449e4
Grelory <gmpwm99@gmail.com>
2020-11-04 13:43:03 +0100

    override findAll function to make it return List instead of iterable

--- src/main/java/com/codecool/GamLib/repositories/PlatformRepository.java
+++ src/main/java/com/codecool/GamLib/repositories/PlatformRepository.java
@@ -2,10 +2,16 @@ package com.codecool.GamLib.repositories;
 
 import com.codecool.GamLib.model.Platform;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlatformRepository extends CrudRepository<Platform, Long> {
 
-
+    @Override
+    @NonNull
+    List<Platform> findAll();
 }
