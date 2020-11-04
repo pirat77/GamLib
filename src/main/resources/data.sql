96d21f16e7d589d77757dcdedc4ac2eab5b90303
Jasio <mcyanek@gmail.com>
2020-11-04 10:49:32 +0100

    completes list of remarkable 5th generation game studios

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,16 @@
 INSERT INTO studio (name, found_date, country, studio_logo) values
 ('Konami', '1969-03-21', 'Japan', 'images/Konami.png'),
-('Capcopm', '1979-05-30', 'Japan', 'images/Kapkom.png'),
+('Capcom', '1979-05-30', 'Japan', 'images/Capcom.png'),
 ('Sony Interactive Entertainment', '1993-11-16', 'Japan', 'images/SIE.png'),
-('Namco', '1955-06-01', 'Japan', 'images/Namco.png');
+('Namco', '1955-06-01', 'Japan', 'images/Namco.png'),
+('Electronic Arts', '1982-05-27', 'USA', 'images/EA.png'),
+('Polyphony Digital', '1994-12-16', 'Japan', 'image/Polyphony.png'),
+('Nintendo', '1889-08-23', 'Japan', 'image/Nintendo.png'),
+('Sega', '1960-06-03', 'Japan', 'image/Sega.png'),
+('Game Freak', '1989-04-26', 'Japan', 'images/GameFreak.png'),
+('Treasure', '1992-06-19', 'Japan', 'images/Treasure.png'),
+('Core Design', '1988-05-13', 'England', 'images/CoreDesign.png'),
+('Activision', '1979-10-01', 'USA', 'images/Activision.png');
 
 INSERT INTO platform (name, cpu_name, cpu_clock_speed, memory, internal_storage, platform_logo) values
 ('Playstation', 'R3000', 33.8688, 3, 0, 'console_logo/playstation.img'),
