7a4aa839d347bf0aacd8890816fa34866142a550
Grelory <gmpwm99@gmail.com>
2020-11-06 00:32:35 +0100

    remove functions which won't work

--- src/main/java/com/codecool/GamLib/repositories/GameRepository.java
+++ src/main/java/com/codecool/GamLib/repositories/GameRepository.java
@@ -1,7 +1,6 @@
 package com.codecool.GamLib.repositories;
 
 import com.codecool.GamLib.model.Game;
-import com.codecool.GamLib.model.Genre;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,5 @@ import java.util.List;
 @Repository
 public interface GameRepository extends CrudRepository<Game, Long> {
 
-    List<Game> findByStudio(String studio);
     List<Game> findByTitle(String title);
-    List<Game> findByGenre(Genre genre);
 }
