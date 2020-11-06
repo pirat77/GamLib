4df4a1d586d88e4f4810357f6bf0024e7a4bc59b
Jasio <mcyanek@gmail.com>
2020-11-06 09:28:36 +0100

    adds coresponding findById methods to repositories

--- src/main/java/com/codecool/GamLib/repositories/GameRepository.java
+++ src/main/java/com/codecool/GamLib/repositories/GameRepository.java
@@ -2,14 +2,18 @@ package com.codecool.GamLib.repositories;
 
 import com.codecool.GamLib.model.Game;
 import com.codecool.GamLib.model.Genre;
+import com.codecool.GamLib.model.Studio;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GameRepository extends CrudRepository<Game, Long> {
 
+    @Override
+    Optional<Game> findById(Long id);
     List<Game> findByTitle(String title);
     List<Game> findByGenre(Genre genre);
 }
--- src/main/java/com/codecool/GamLib/repositories/PlatformRepository.java
+++ src/main/java/com/codecool/GamLib/repositories/PlatformRepository.java
@@ -1,10 +1,12 @@
 package com.codecool.GamLib.repositories;
 
 import com.codecool.GamLib.model.Platform;
+import com.codecool.GamLib.model.Studio;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PlatformRepository extends CrudRepository<Platform, Long> {
@@ -13,6 +15,8 @@ public interface PlatformRepository extends CrudRepository<Platform, Long> {
     @NonNull
     List<Platform> findAll();
 
+    @Override
+    Optional<Platform> findById(Long id);
     List<Platform> findByName(String name);
     List<Platform> findByCpuName(String cpuName);
 }
